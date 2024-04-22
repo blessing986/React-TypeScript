b996ec94e3bdb21cb22a6ae31b4817ee4fd26113
Blessing U <blessing986@users.noreply.github.com>
2024-04-22 16:49:35 +0100

    chore: typing in an array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
-    first:"Blessing",
-    last: "Ubiomor"
-  }
+    first: "Blessing",
+    last: "Ubiomor",
+  };
+
+  const nameList = [
+    {
+      first: "Blessing",
+      last: "Ubiomor",
+    },
+    {
+      first: "Teddy",
+      last: "Teddy",
+    },
+    {
+      first: "Mercy",
+      last: "Ubiomor",
+    },
+  ];
+
   return (
     <div className="App">
       <Greet name="Blessing" messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
