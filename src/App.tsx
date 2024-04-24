af1540fec500e8128b2a672b9120f35c559f694f
Blessing U <blessing986@users.noreply.github.com>
2024-04-24 13:03:46 +0100

    chore: event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
 import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
@@ -29,14 +31,21 @@ function App() {
 
   return (
     <div className="App">
-       <Greet name="Blessing" isLoggedIn={false} />
-     {/* <Person name={personName} />
-      <PersonList names={nameList} /> */}
+      {/*  <Greet name="Blessing" isLoggedIn={false} />
+     <Person name={personName} />
+      <PersonList names={nameList} /> 
       <Status status="loading" />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicpario!</Heading>
-      </Oscar>
+      </Oscar>*/}
+
+      <Button
+        handleclick={(event, id) => {
+          console.log("Button clicked", { event }, { id });
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
